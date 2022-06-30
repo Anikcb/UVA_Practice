e4262a59b3c650037e32c957297c281cd7d56943
Anik chakraborty <anikchakraborty562@gmail.com>
2022-06-30 15:44:13 +0600

    Create 195.cpp

---
+++ Solutions/195.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ull                    unsigned long long int
+#define          db                     double
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     3.141592653589793238462643
+#define          rep( i , a , b )       for( i=a ; i<b ; i++)
+#define          rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define          repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define          RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          get_pos(c,x)           (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          CASEL(t)               printf("Case %d:\n",t)
+#define          Unique(X)             (X).erase(unique(all(X)),(X).end())
+#define          tc1(x)                 printf("Case %d: ",x)
+#define          tc2(x)                 printf("Case #%d: ",x)
+#define          tc3(x)                 printf("Case %d:\n",x)
+#define          tc4(x)                 printf("Case #%d:\n",x)
+
+bool comparator(char a, char b){
+	if(tolower(a) == tolower(b)){
+		return a<b;
+	}
+	return tolower(a) < tolower(b);
+}
+int main()
+{
+
+    CIN;
+
+    string s,s1;
+    ll n;
+    cin>>n;
+    while(n--)
+    {
+        cin>>s;
+        sort(s.begin(),s.end(),comparator);
+        do
+        {
+            cout<<s<<endl;
+        }
+        while(next_permutation(s.begin(),s.end(),comparator));
+         s.clear();
+
+    }
+
+
+    return 0;
+}
